bde4a9d8d7fc6758e8b9be46dd6f79448a5f8199
JayaJangid-08 <jayajangiddhamu05@gmail.com>
2025-12-26 15:01:55 +0530

    Initial commit

--- Rent_calculate.py
+++ Rent_calculate.py
@@ -14,3 +14,8 @@ total_rent = float(input("Enter the total rent amount: "))
 total_food = float(input("Enter the total food ordered amount: "))
 electricity_units = float(input("Enter the electricity units spent: "))
 charge_per_unit = float(input("Enter the charges per unit of electricity: "))
+Persons = int(input("Enter the number of persons sharing the rent: "))
+
+total_bill = electricity_units * charge_per_unit
+output = (total_rent + total_food + total_bill) // Persons
+print(f"The total amount you've to pay is: {output}")
