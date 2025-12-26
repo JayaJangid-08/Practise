f8a6bf0442356b2d80e6059a61b41efe01cfa359
JayaJangid-08 <jayajangiddhamu05@gmail.com>
2025-12-26 14:57:58 +0530

    Initial commit

--- Rent_calculate.py
+++ Rent_calculate.py
@@ -9,4 +9,8 @@ Input we need
 Output
     ->Total amount yo've to pay is
 '''
-input()
+input("Welcome to Rent Calculator\nPress Enter to continue")
+total_rent = float(input("Enter the total rent amount: "))
+total_food = float(input("Enter the total food ordered amount: "))
+electricity_units = float(input("Enter the electricity units spent: "))
+charge_per_unit = float(input("Enter the charges per unit of electricity: "))
