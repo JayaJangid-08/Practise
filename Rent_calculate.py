6bd6709f48ce83e940dc2eac65a5016fb8c187c5
JayaJangid-08 <jayajangiddhamu05@gmail.com>
2025-12-26 14:47:11 +0530

    Initial commit

---
+++ Rent_calculate.py
@@ -0,0 +1,12 @@
+'''
+Input we need
+        -> Total rent
+        -> Total food ordered
+        -> Electricity units spend 
+        -> Charges per unit
+'''
+'''
+Output
+    ->Total amount yo've to pay is
+'''
+input()
